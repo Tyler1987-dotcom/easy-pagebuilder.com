1ea7086302ab8f0aca8a14ac108e9cfa15073a1c
Your Name <you@example.com>
2024-12-19 17:31:14 -0700

    Your commit message

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,14 +1,14 @@
-{
-  "presets"; [
+module.exports = {
+  "presets": [
     "@babel/preset-env",
     "@babel/preset-react"
   ],
- "plugins"; [
+  "plugins": [
     "@babel/plugin-transform-class-properties",
-    "@babel/plugin-transform-private-methods",
     "@babel/plugin-transform-private-property-in-object",
     "@babel/plugin-transform-optional-chaining",
     "@babel/plugin-transform-nullish-coalescing-operator",
+    '@babel/plugin-proposal-private-methods',
     "@babel/plugin-transform-numeric-separator"
   ]
 }
