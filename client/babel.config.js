f4c2ae846efa9a288c70160034559055a9f94574
Your Name <you@example.com>
2024-12-19 19:13:41 -0700

    Fix Babel plugin configuration

--- client/babel.config.js
+++ client/babel.config.js
@@ -4,11 +4,11 @@ module.exports = {
     "@babel/preset-react"
   ],
   "plugins": [
+    "@babel/plugin-proposal-private-methods", // Corrected plugin
+    "@babel/plugin-proposal-private-property-in-object", // Corrected plugin
     "@babel/plugin-transform-class-properties", // Updated plugin
-    "@babel/plugin-transform-private-property-in-object", // Updated plugin
     "@babel/plugin-transform-optional-chaining", // Updated plugin
     "@babel/plugin-transform-nullish-coalescing-operator", // Updated plugin
-    "@babel/plugin-transform-private-methods", // Updated plugin
     "@babel/plugin-transform-numeric-separator"
   ]
 }
